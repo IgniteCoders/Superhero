d8de8f33e468bd6a81ba90b5be67acc1a3275d64
Mansour <theboom.kajas@gmail.com>
2024-11-13 19:03:03 +0100

    Designed the item_superhero.xml to improve user experience.

--- app/src/main/java/com/example/superhero/activities/MainActivity.kt
+++ app/src/main/java/com/example/superhero/activities/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.superhero.activities
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.superhero.R
@@ -26,15 +29,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         adapter = SuperheroAdapter(superheroList)
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
 
-        searchSuperheroes("")
+        searchSuperheroes("a")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
